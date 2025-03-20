16e48fe3ad4d372d20b9fd6b8151e43380f8df91
KevinTan8318 <k.tan14@share.epsb.ca>
2025-03-20 13:29:54 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,81 @@
+document.getElementById("btn").addEventListener("click", Idioms);
+document.getElementById("btn2").addEventListener("click", Idioms2);
+document.getElementById("btn3").addEventListener("click", Idioms3);
+document.getElementById("btn4").addEventListener("click", Idioms4);
+document.getElementById("btn5").addEventListener("click", Idioms5);
+document.getElementById("btn6").addEventListener("click", Idioms6);
+document.getElementById("btn7").addEventListener("click", Idioms7);
+document.getElementById("btn8").addEventListener("click", Idioms8);
+
+document.getElementById("btn9").addEventListener("click", Idioms9);
+document.getElementById("btn10").addEventListener("click", Idioms10);
+document.getElementById("btn11").addEventListener("click", Idioms11);
+document.getElementById("btn12").addEventListener("click", Idioms12);
+document.getElementById("btn13").addEventListener("click", Idioms13);
+document.getElementById("btn14").addEventListener("click", Idioms14);
+document.getElementById("btn15").addEventListener("click", Idioms15);
+document.getElementById("btn16").addEventListener("click", Idioms16);
+
+function Idioms() {
+  alert("there");
+}
+
+function Idioms2() {
+  alert("the fat lady sings");
+}
+
+function Idioms3() {
+  alert("know one");
+}
+
+function Idioms4() {
+  alert("dogs");
+}
+
+function Idioms5() {
+  alert("one stone");
+}
+
+function Idioms6() {
+  alert("ice");
+}
+
+function Idioms7() {
+  alert("the room");
+}
+
+function Idioms8() {
+  alert("its cover");
+}
+
+function Idioms9() {
+  alert("some eggs");
+}
+
+function Idioms10() {
+  alert("make him drink");
+}
+
+function Idioms11() {
+  alert("with honey than you can with vinegar");
+}
+
+function Idioms12() {
+  alert("it pours");
+}
+
+function Idioms13() {
+  alert("storm");
+}
+
+function Idioms14() {
+  alert("eye");
+}
+
+function Idioms15() {
+  alert("money");
+}
+
+function Idioms16() {
+  alert("sailed");
+}
